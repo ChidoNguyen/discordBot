47713abdeb85ffd62d33a5cac9be8b4e7aa7eefe
Chido Nguyen <cheeds@Chidos-Air.hsd1.or.comcast.net>
2024-06-12 00:56:27 -0700

    basic outline of bot login

--- botFilez/testbot.py
+++ botFilez/testbot.py
@@ -1,17 +1,24 @@
 import discord
 import discordCreds as creds
 
-client =  discord.Client()
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-	print('We have logged in as {0.user}'.format(client))
+    print(f'We have logged in as {client.user}')
 
 @client.event
 async def on_message(message):
-	if message.author == client.user:
-		return
-	if message.content.startswith('$hello'):
-		await message.channel.send('Hello!')
+    if message.author == client.user:
+        return
 
-client.run(creds.Discord)
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(creds.myDiscordCreds)
+
