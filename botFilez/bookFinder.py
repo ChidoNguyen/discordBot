2d24a9a99abc320f8cdaecd072050707f310be1e
Chido Nguyen <cheeds@Chidos-MacBook-Air.local>
2024-06-12 11:51:15 -0700

    sourcing all links on zlib

--- botFilez/bookFinder.py
+++ botFilez/bookFinder.py
@@ -12,7 +12,17 @@ from discordCreds import siteURL , userID , userPASS
 driver = webdriver.Chrome()
 driver.get(siteURL) #fill with our url
 
+try:
+    assert "Z-Library" in driver.title
+except:
+    print(f'Bad URL : {siteURL}')
 
+#scoure the site for "a href" so we can go thru it for login redirection
+aHref = driver.find_element(By.TAG_NAME,'a href')
+
+for a in aHref:
+    if "login" in a:
+        print(a)
 
 
 driver.close()
