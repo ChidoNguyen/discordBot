a8d2881906a906c424ed78d87f87422096b8d0c0
Chido Nguyen <cheeds@Chidos-MacBook-Air.local>
2024-06-12 12:11:57 -0700

    using firefox website to get code flow for webdriver link extraction

--- botFilez/bookFinder.py
+++ botFilez/bookFinder.py
@@ -10,19 +10,21 @@ from discordCreds import siteURL , userID , userPASS
 
 
 driver = webdriver.Chrome()
-driver.get(siteURL) #fill with our url
+driver.get('https://www.mozilla.org/en-US/firefox/new/?redirect_source=firefox-com') #fill with our url
 
-try:
-    assert "Z-Library" in driver.title
-except:
-    print(f'Bad URL : {siteURL}')
+# try:
+#     assert "Z-Library" in driver.title
+# except:
+#     print(f'Bad URL : {siteURL}')
 
 #scoure the site for "a href" so we can go thru it for login redirection
-aHref = driver.find_element(By.TAG_NAME,'a href')
+aHref = driver.find_elements(By.TAG_NAME,'a')
 
-for a in aHref:
-    if "login" in a:
-        print(a)
+loginURL = ""
 
-
+#links = [href.get_attribute('href') for items in aHref]
+#print(links)
+for items in aHref:
+    if "login" in (items.get_attribute('href')).lower():
+        print("found login")
 driver.close()
