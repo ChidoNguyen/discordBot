03e28ce5d5a1b616219093d67178989114fe15f5
Chido Nguyen <cheeds@Chidos-MacBook-Air.local>
2024-06-14 12:29:09 -0700

    linkProcessing currently : identifies download button element

--- botFilez/linkProcessing.py
+++ botFilez/linkProcessing.py
@@ -1,1 +1,22 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException , NoSuchElementException
+
+PREF = {"download_dir" : ""}
+#url link input
+def lp(driver):
+    bookURL = "https://z-library.rs/book/28168813/d7121c/the-anxious-generation.html"
+
+    #driver = webdriver.Chrome()
+    driver.get(bookURL)
+    try:
+        wait = WebDriverWait(driver,10)
+        wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "a.btn.btn-primary.addDownloadedBook")))
+        driver.alert()
+    except NoSuchElementException:
+        print("failed download")
+
+    return
