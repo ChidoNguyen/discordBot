c17e1fe3b5156bfb10c7cb4ce993321aabfecb26
Chido Nguyen <cheeds@Chidos-MacBook-Air.local>
2024-07-01 12:39:36 -0700

    multiprocess .terminate() after bot join to kill webserver

--- botFilez/bookAPI.py
+++ botFilez/bookAPI.py
@@ -10,7 +10,6 @@ from discordCreds import desired_save_dir
 
 app = Flask(__name__)
 server = None
-shutdown_event = None
 @app.route('/search_download/', methods = ['POST'])
 def search_download():
         book_details = request.json['book_info']
@@ -31,25 +30,12 @@ def cleanup():
     except:
         print("nothing to remove")
     return "Clean up finished." , 200
-    
 
-@app.route('/shutdown/',methods = ['POST'])
-def shutdown():
-    shutdown_server()
-    return 'Shutting down...', 200
-
-def shutdown_server():
-    global shutdown_event
-    shutdown_event.set()
-    time.sleep(2)
-
-def spin_up(event):
+def spin_up():
     global server
-    global shutdown_event
-    shutdown_event = event
+    print("API initiating.")
     server = make_server('127.0.0.1',5000,app)
     server.serve_forever()
 
 if __name__ == '__main__':
-    print("API spun up.")
     spin_up()
