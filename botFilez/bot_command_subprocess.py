4f98f257207e8faca40c11c7d3e27bc91cb4a48c
Chido Nguyen <cheeds@Chidos-MacBook-Air.local>
2024-07-18 13:40:19 -0700

    auto and listing works ; need to tweak url ; might need to redo limit reached check

--- botFilez/bot_command_subprocess.py
+++ botFilez/bot_command_subprocess.py
@@ -3,9 +3,81 @@ from discord.ext import tasks
 import requests , json
 import os , time , datetime
 import concurrent.futures
+import discordCreds as creds
 
+API_END = { 'api' : "http://localhost:5000/" ,
+            'download' : "search_download/",
+            'listing' : "search_links/",
+            'pick' : "download_url/",
+            'cleanup' : "cleanup"
+           }
+#search folder should only have 1 epub currently, grabs it and create file for discord bot to attach/upload
+def discord_file_creation():
+    myfile = None
+    for items in os.listdir(creds.desired_save_dir):
+        if items.endswith('epub'):
+            myfile = items
+    joined_path = os.path.join(creds.desired_save_dir , myfile)
+    with open(joined_path , 'rb') as discordFile:
+        attached_file = discord.File(fp = discordFile , filename=myfile)
+    return attached_file
 
 
 #download_book for !getbook
+'''
+param search_str : parsed message user input for bot to search has the !command portion removed
+param requester : is a discord message object to identify user id/name
+'''
+def download_book(search_str , requester ):
+    #request book
+    #make file
+    #if successful send info back
+    #else error messages
+    data = {'book_info' : search_str}
+    try:
+        response = requests.post(API_END['api'] + API_END['download'] , json=data)
+    except:
+        print(f'{response.text}')
+    if response.status_code == 200:
+        book_file = discord_file_creation()
+        return book_file , f"{requester.mention}"
+    elif response.status_code == 405:
+        return f'Download limit reached. {requester.mention}'
+    else:
+        return f'Failed to get book : {search_str} {requester.mention}'
 
-def download_book(search_str , )
+
+def search_results(search_str,requester,user_state):
+    data = {'book_info' : search_str}
+    try:
+        response = requests.post(API_END['api'] + API_END['listing'] , json=data)
+    except:
+        print(f'{response.text}')
+    json_response_payload = json.loads(response.content)
+    processed_response = json_response_payload['data'].split()
+    if not processed_response:
+        return f'No results were found. {requester.mention}'
+    elif response.status_code == 200:
+        user_state.book_options = processed_response
+        format_msg = ""
+        for idx,items in enumerate(processed_response , start = 1):
+            format_msg += f'{idx}. <{items}>\n'
+        format_msg += f'{requester.mention}'
+
+        return format_msg
+    elif response.status_code == 400:
+        return f'Bad results file. {requester.mention}'
+    
+
+def search_results_download(user_choice , requester , user_state):
+    user_choice_url = user_state.book_options[user_choice]
+    data = {'book_info' : user_choice_url}
+    response = requests.post(API_END['api'] + API_END['pick'] , json=data)
+    if response.status_code == 200:
+        file_obj = discord_file_creation()
+        return file_obj, f'{requester.mention}'
+    elif response.status_code == 405:
+        return f'Download limit reached. {requester.mention}'
+    else:
+        return f'Failed to get book. {requester.mention}'
+    
--- botFilez/testbot.py
+++ botFilez/testbot.py
@@ -7,6 +7,7 @@ import json
 import time,datetime
 import concurrent.futures
 
+from bot_command_subprocess import download_book , search_results , search_results_download
 #bot permissions#
 intents = discord.Intents.default()
 intents.message_content = True
@@ -18,15 +19,22 @@ executor = concurrent.futures.ThreadPoolExecutor(max_workers=4)
 
 #some housekeeping items and discord server specific for internal usage#
 GUILD = "Janitors Guild"
-API_ENDPOINT = { 'api' : "http://localhost:5000/"}
+API_ENDPOINT = { 
+    'api' : "http://localhost:5000/",
+    'cleanup' : "http://localhost:5000/cleanup/"
+        }
 ALLOWED_CHANNEL = ['im-testing-shit-ignore-me-chido','book-club']
 USER_ID = {'kkot' : 705999688893071430, 'jonathan': 137004891360067584}
 COMMAND_PREFIX = "!"
+#commands
 commands ={}
-
+def command(name):
+    def decorator(func):
+        commands[name] = func
+        return func
+    return decorator
 #user states
 user_states = {}
-
 class UserStates :
     def __init__(self):
         self.cancel_flag = False
@@ -34,11 +42,7 @@ class UserStates :
         self.book_options = []
         self.timestamp = None
 
-def command(name):
-    def decorator(func):
-        commands[name] = func
-        return func
-    return decorator
+
 def clean_up_users():
     current_time = datetime.now()
     for ppl in user_states:
@@ -98,43 +102,16 @@ async def helper(message):
     await message.channel.send(multi_line_msg)
 @command('getbook')
 async def get_book(message):
-    #basic info
-    #requester = message.author
-    url_path = "search_download/"
-    
-    #send basic message for user experience knowing bot is doing something
     await message.channel.send('\U0001F50E')
-    
-    #parse message info
-    #who
     requester = message.author
-    #what was sent
     parsed_msg = message.content.split() #split by white spaces
-    #ignore the bot command which is first item in parsed_msg
     search_string = ' '.join(parsed_msg[1:])
     
-    def download_book(search_str): #hopefully prevents blocking of discord heartbeat 
-        data = {"book_info" : search_str}
-        try:
-            response = requests.post(API_ENDPOINT['api'] + url_path, json=data)
-        except:
-            print(response.text)
-        
-        if response.status_code == 200:
-            file_obj = discord_file_creation()
-            return file_obj , f"{requester.mention}"
-        elif response.status_code == 405:
-            return f'Download limit reached. {requester.mention}'
-        else:
-            return f'Failed to get book: {search_str} {requester.mention}'
-    
-    
-    future = executor.submit(download_book,search_string) #run in sep. thread
-    
+    #hoping this unblocks the discord bot from timing out while waiting for it to finish
+    future = executor.submit(download_book,search_string,requester)
     result = await client.loop.run_in_executor(None , future.result)
     
     if isinstance(result , tuple):
-        #if our result comes back with file + message aka a tuble
         file_obj , msg = result
         await message.channel.send("File: ", file=file_obj)
         await message.channel.send(msg)
@@ -145,82 +122,53 @@ async def get_book(message):
 @command('tellmeajoke')
 async def tell_joke(message):
     await message.channel.send(f"look in the mirror {message.author.mention}!")
-# todo : HANDLE FILE LARGE EXCEPTION
-@command('getbook1')
-async def get_book1(message):
-    await message.channel.send('\U0001F50E')
-    url_path = "search_download/"
-    requester = message.author
-    #adding user state
-
-    #message is the original bot command !getbook author title
-    message_parsed = message.content.split()
-    #ignore the first item
-    search_string = ' '.join(message_parsed[1:])
-    #requests post
-    data = {"book_info" : search_string}
-    try:
-        response = requests.post(API_ENDPOINT['api'] + url_path , json = data)
-        #print(data)
-    except:
-        print("f response")
-
-    ##### code below will be moved to "download" as pect later
-    if response.status_code == 200:
-        file_obj = discord_file_creation()
-        try:
-            await message.channel.send("File: ", file = file_obj)
-        except discord.HTTPException as e:
-            print(e)
-        finally:
-            await message.channel.send(f"{message.author.mention}")
-            requests.get('http://localhost:5000/cleanup')
-    elif response.status_code == 405:
-        print(response.text)
-        await message.channel.send(f'Download limit reached. {requester.mention}')
-    else:
-        print(response.text)
-        await message.channel.send(f'Failed to !getbook : {search_string} {requester.mention}')
 @command('getbook-adv')
 async def getbook_adv(message):
-    url_path = "search_links/"
+    #url_path = "search_links/"
     requester = message.author
-    #adding user state
     if requester not in user_states:
         user_states[requester] = UserStates()
+    #saving timestamp for future cleanup usage(?)
     state = user_states[requester]
     state.timestamp = datetime.datetime.now()
 
-    async def process_user_query():
-        #message is the original bot command !getbook author title
-        message_parsed = message.content.split()
-        #ignore the first item
-        search_string = ' '.join(message_parsed[1:])
-        #requests post
-        data = {"book_info" : search_string}
-        try:
-            response = requests.post(API_ENDPOINT['api'] + url_path, json = data)
-            #print(data)
-        except:
-            print("f response")
+    parse_message = message.content.split()
+    search_string = ' '.join(parse_message[1:])
 
-        if state.cancel_flag == True:
-            await message.channel.send("Book request canned.")
-            return
-        
-        unprocessed_response_payload= json.loads(response.content)
-        state.book_options = unprocessed_response_payload['data'].split()
-        my_msg = ""
-        if not state.book_options:
-            my_msg = "No results were found."
-        else:
-            for idx,items in enumerate(state.book_options , start = 1):
-                my_msg += f'{idx}. <{items}>\n'
-        
-        await message.channel.send(my_msg)
+    future = executor.submit(search_results,search_string,requester,state)
+    state.task = await client.loop.run_in_executor(None, future.result)
+    result = state.task
+    await message.channel.send(result)
+    # async def process_user_query():
+    #     #message is the original bot command !getbook author title
+    #     message_parsed = message.content.split()
+    #     #ignore the first item
+    #     search_string = ' '.join(message_parsed[1:])
+    #     #requests post
+    #     data = {"book_info" : search_string}
+    #     try:
+    #         response = requests.post(API_ENDPOINT['api'] + url_path, json = data)
+    #         #print(data)
+    #     except:
+    #         print("f response")
 
-    state.task = client.loop.create_task(process_user_query())
-    return
+    #     if state.cancel_flag == True:
+    #         await message.channel.send("Book request canned.")
+    #         return
+        
+    #     unprocessed_response_payload= json.loads(response.content)
+    #     state.book_options = unprocessed_response_payload['data'].split()
+    #     my_msg = ""
+    #     if not state.book_options:
+    #         my_msg = "No results were found."
+    #     else:
+    #         for idx,items in enumerate(state.book_options , start = 1):
+    #             my_msg += f'{idx}. <{items}>\n'
+        
+    #     await message.channel.send(my_msg)
+
+    # state.task = client.loop.create_task(process_user_query())
+    # return
 @command('pick')
 async def pick_book(message):
     error_msg = {
@@ -229,8 +177,9 @@ async def pick_book(message):
         'invalid_num' : 'Please enter a number.',
         'task' : 'There is no book links attached to you.'
     }
-    api_path = "download_url/"
+
     requester = message.author
+    #check if user has ran a listings request yet
     try:
         state = user_states[requester]
     except:
@@ -252,32 +201,40 @@ async def pick_book(message):
         await message.channel.send(error_msg['invalid'])
     elif (0 >= int(parsed_msg[1]) or int(parsed_msg[1]) > len(state.book_options)):
         await message.channel.send(error_msg['invalid_choice'])
-        
     else:
         await message.channel.send("Sit tight.")
         user_choice = int(parsed_msg[-1]) - 1 # convert to 0 index
-        choice_url = state.book_options[user_choice]
-        data = {'book_info' : choice_url}
-
-        response = requests.post(API_ENDPOINT['api'] + api_path , json = data)
-
-        if response.status_code == 200:
-            file_obj = discord_file_creation()
-            try:
-                await message.channel.send("File: ", file = file_obj)
-            except discord.HTTPException as e:
-                print(e)
-            await message.channel.send(f"{message.author.mention}")
-            #reset user states/tasks
+        future = executor.submit(search_results_download,user_choice,requester,state)
+        result = await client.loop.run_in_executor(None, future.result)
+        if isinstance(result, tuple):
+            f , msg = result
+            await message.channel.send("File: ", file=f)
+            await message.channel.send(msg)
             state.task = None
             state.book_options = []
-            requests.get('http://localhost:5000/cleanup')
-        elif response.status_code == 405:
-            print(response.text)
-            await message.channel.send(f'Download limit reached.')
         else:
-            print(response.text)
-            await message.channel.send(f'Failed to get book.')
+            await message.channel.send(result)
+    requests.get(API_ENDPOINT['cleanup'])
+###############
+        # response = requests.post(API_ENDPOINT['api'] + api_path , json = data)
+
+        # if response.status_code == 200:
+        #     file_obj = discord_file_creation()
+        #     try:
+        #         await message.channel.send("File: ", file = file_obj)
+        #     except discord.HTTPException as e:
+        #         print(e)
+        #     await message.channel.send(f"{message.author.mention}")
+        #     #reset user states/tasks
+        #     state.task = None
+        #     state.book_options = []
+        #     requests.get('http://localhost:5000/cleanup')
+        # elif response.status_code == 405:
+        #     print(response.text)
+        #     await message.channel.send(f'Download limit reached.')
+        # else:
+        #     print(response.text)
+        #     await message.channel.send(f'Failed to get book.')
 
 @command('cancel')
 async def cancel(message):
