bd7848d7912812a3c1d134a86d9aae5e59a93b7c
Chido Nguyen <cheeds@Chidos-MacBook-Air.local>
2024-07-18 12:25:44 -0700

    splitting bot commands to functions for subprocess

---
+++ botFilez/bot_command_subprocess.py
@@ -0,0 +1,11 @@
+import discord
+from discord.ext import tasks
+import requests , json
+import os , time , datetime
+import concurrent.futures
+
+
+
+#download_book for !getbook
+
+def download_book(search_str , )
