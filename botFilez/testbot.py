197381974735d4aa190a6db6478bc7b9c1b920c1
Chido Nguyen <chidonguyen@hotmail.com>
2024-07-19 11:34:48 -0700

    reworked getbook

--- botFilez/testbot.py
+++ botFilez/testbot.py
@@ -195,7 +195,7 @@ async def pick_book(message):
         'invalid_num' : 'Please enter a number.',
         'task' : 'There is no book links attached to you.'
     }
-    reply_thread = message.fetch_thread
+    reply_thread = message.fetch_thread()
     requester = message.author
     #check if user has ran a listings request yet
     try:
